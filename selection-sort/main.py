0be1ce6237556bdd9e3de2a4de441900e80245fa
GlauberBalsani <glauber_29@hotmail.com>
2022-08-20 22:23:07 -0300

    remove

--- selection-sort/main.py
+++ selection-sort/main.py
@@ -1,14 +1,21 @@
 
+#selection sort
+
+arr = [3, 2, 1]
+
+for i in range(len(arr)):
+     minor = i
+
+     for j in range(i + 1, len(arr)):
+         if arr[j] < arr[minor]:
+             minor = j
+         if arr[i] != arr[minor]:
+            aux = arr[i]
+            arr[i] = arr[minor]
+            arr[minor] = aux
+
+print(arr)
 
 
-def selectionSort(arr):
-    minor  = arr[0]
-    for i in range(len(arr)):
-        if arr[i] <  minor:
-            minor = arr[i]
-    return minor
-
-
-print(selectionSort([3, 5, 7, 10, 0, 2]))
 
 
